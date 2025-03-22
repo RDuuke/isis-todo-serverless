e44997d23a392c4f46a80d7b8154a303af2e546f
Juan Duque <juuanduuke@gmail.com>
2025-03-21 19:59:06 -0500

    Update main

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,20 @@ import { CreateTodoUseCase } from "./core/application/create-todo.usecase";
 import { TodoMockRepository } from "./core/infrastructure/repository/todo.mock.repository";
 import { CreateController } from "./web/controllers/create.controller";
 import { createTodoRoute } from "./web/routes/create.routes";
+import { listTodoRoute } from "./web/routes/list.routes";
+import { ListController } from "./web/controllers/list.controller";
+import { GetAllTodoUsecase } from "./core/application/get-all-todo.usecase";
 
 const repo = new TodoMockRepository();
-const usecase = new CreateTodoUseCase(repo);
-const controller = new CreateController(usecase);
+const create_usecase = new CreateTodoUseCase(repo);
+const create_controller = new CreateController(create_usecase);
+const get_usecase = new GetAllTodoUsecase(repo);
+const list_controller = new ListController(get_usecase);
 
 const app = express();
 app.use(cors({ origin: 'http://localhost:4200' }));
 
 app.use(express.json());
-app.use("/api", createTodoRoute(controller));
+app.use("/api", createTodoRoute(create_controller));
+app.use("/api", listTodoRoute(list_controller));
 app.listen(3000, () => console.log("Server is running on port 3000"));
