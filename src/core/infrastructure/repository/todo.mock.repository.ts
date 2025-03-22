8df1165bacd79a49037b41d56441441735674fc2
Juan Duque <juuanduuke@gmail.com>
2025-03-21 19:58:36 -0500

    Add usecase also update repository

--- src/core/infrastructure/repository/todo.mock.repository.ts
+++ src/core/infrastructure/repository/todo.mock.repository.ts
@@ -1,6 +1,7 @@
 import { TodoItem } from "../../domain/todo.model";
+import { TodoRepository } from "../../domain/todo.repository";
 
-export class TodoMockRepository {
+export class TodoMockRepository implements TodoRepository {
     private todos: TodoItem[] = [];
     private idCounter = 1;
 
@@ -13,4 +14,9 @@ export class TodoMockRepository {
         this.todos.push(todo);
         return todo;
     }
+
+    async get(): Promise<TodoItem[]> {
+        return this.todos;
+    }
+
 }
